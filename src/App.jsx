150b2b8cc7549584cca35323d7336bdae32dcfec
nominoman16hc046 <nominoman16hc046@gmail.com>
2025-12-31 00:03:30 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <RobotScene />
+      {/* <RobotScene /> */}
       <Hero />
       <Services />
       <ContactForm />
